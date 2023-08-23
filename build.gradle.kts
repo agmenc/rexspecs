65274692ce3f34d8a547faeb60224b826d6a6a63
agmenc <chris.agmen-smith@email.com>
2023-08-23 14:15:05 +0100

    Start this chain, so that we can use the latest HTTP4K: upgrade plugin [done] -> upgrade Gradle [started] -> upgrade Kotlin version to 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "6.8"
+    gradleVersion = "6.8.3"
 //    gradleVersion = "8.1.1"
 }
 
@@ -16,6 +16,14 @@ repositories {
 }
 
 dependencies {
+
+    // SystemUnderTest
+//    implementation(platform("org.http4k:http4k-bom:5.7.2.0"))
+//    implementation("org.http4k:http4k-core")
+//    implementation("org.http4k:http4k-server-undertow")
+//    implementation("org.http4k:http4k-client-apache")
+
+
 //    testImplementation(kotlin("test"))
 //    testImplementation("com.natpryce:hamkrest:1.8.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.0.0")
