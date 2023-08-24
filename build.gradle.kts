d448ea0da1cd5e70d15b647846aa4068f3a12223
agmenc <chris.agmen-smith@email.com>
2023-08-24 22:19:17 +0100

    Start this chain, so that we can use the latest HTTP4K: upgrade plugin [done] -> upgrade Gradle [actually done] -> upgrade Kotlin  version to 1.9.0 [done]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.20"
-//        kotlin("jvm") version "1.9.0"
+//    kotlin("jvm") version "1.6.20"
+        kotlin("jvm") version "1.9.0"
     application
 }
 
